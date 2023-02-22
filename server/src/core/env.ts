bc4f73f18c5a97faf6231575fb11e422807aa086
chihabMg <chihab.mg.me@gmail.com>
2023-02-22 21:43:00 +0100

    added refresh/access secret getters

--- server/src/core/env.ts
+++ server/src/core/env.ts
@@ -1,5 +1,23 @@
 const PORT = process.env.PORT ?? 3001;
+const isProduction = () => {
+  return process.env.MODE == "PRODUCTION";
+};
+const getRefreshTokenSecret = () => {
+  const secret = process.env.REFRESH_SECRET;
+  if (!secret)
+    throw new Error("please provide a REFRESH_SECRET as env variable");
+  return secret;
+};
+const getAccessSecret = () => {
+  const secret = process.env.REFRESH_SECRET;
+  if (!secret)
+    throw new Error("please provide a ACCESS_SECRET as env variable");
+  return secret;
+};
 
 export default {
   PORT,
+  isProduction,
+  getRefreshTokenSecret,
+  getAccessSecret,
 };
