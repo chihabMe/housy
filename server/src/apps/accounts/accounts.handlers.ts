e896bbc8ae6e47f7105a7826bd542a1eaa90d812
chihabMg <chihab.mg.me@gmail.com>
2023-02-22 21:52:28 +0100

    added accounts handler + accounts routes

--- server/src/apps/accounts/accounts.handlers.ts
+++ server/src/apps/accounts/accounts.handlers.ts
@@ -7,3 +7,23 @@ export const accountsRegisterHandler = (req: Request, res: Response) => {
 export const accountsMeHandler = (req: Request, res: Response) => {
   res.status(httpStatus.OK).json("me");
 };
+export const accountsDeleteHandler = (req: Request, res: Response) => {
+  res.status(httpStatus.OK).json("delete");
+};
+export const accountsActivateHandler = (req: Request, res: Response) => {
+  res.status(httpStatus.OK).json("activation");
+};
+
+export const accountsUpdateProfileHandler = (req: Request, res: Response) => {
+  res.status(httpStatus.OK).json("update profile");
+};
+
+export const accountsChangePassword = (req: Request, res: Response) => {
+  res.status(httpStatus.OK).json("change password");
+};
+export const accountsChangeEmailHandler = (req: Request, res: Response) => {
+  res.status(httpStatus.OK).json("change email");
+};
+export const accountsRestorePasswordHandler = (req: Request, res: Response) => {
+  res.status(httpStatus.OK).json("restore password");
+};
--- server/src/apps/accounts/accounts.routes.ts
+++ server/src/apps/accounts/accounts.routes.ts
@@ -1,10 +1,34 @@
 import { Router } from "express";
 import protectedRouteMiddleware from "../../core/middlewares/protected.middleware";
 import {
+  accountsActivateHandler,
+  accountsChangeEmailHandler,
+  accountsChangePassword,
+  accountsDeleteHandler,
   accountsMeHandler,
   accountsRegisterHandler,
+  accountsRestorePasswordHandler,
 } from "./accounts.handlers";
 
 export const accountsRouter = Router();
 accountsRouter.get("/register", accountsRegisterHandler);
 accountsRouter.get("/me", protectedRouteMiddleware, accountsMeHandler);
+accountsRouter.get("/restore/password", accountsRestorePasswordHandler);
+
+//
+accountsRouter.get("/delete", protectedRouteMiddleware, accountsDeleteHandler);
+accountsRouter.get(
+  "/activate",
+  protectedRouteMiddleware,
+  accountsActivateHandler
+);
+accountsRouter.get(
+  "/change/email",
+  protectedRouteMiddleware,
+  accountsChangeEmailHandler
+);
+accountsRouter.get(
+  "/change/password",
+  protectedRouteMiddleware,
+  accountsChangePassword
+);
