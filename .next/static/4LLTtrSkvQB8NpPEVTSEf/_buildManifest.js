6102d0b595fa777c191e051296c2bed1280310f7
chihabMg <chihab.mg.me@gmail.com>
2023-03-09 22:16:53 +0100

    fixed nodejs

--- .next/static/4LLTtrSkvQB8NpPEVTSEf/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/50901216b76e581e.css","static/chunks/pages/index-21f46166e59995d3.js"],"/_error":["static/chunks/pages/_error-dd181ac34baa5fce.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
