63f74b355bfffe1edb6617e852045b2ebd8f1380
chihabMg <chihab.mg.me@gmail.com>
2023-03-12 07:25:03 +0100

    added basic login/profile page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,25 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import { useEffect } from "react";
-import { date } from "zod";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [message, setMessage] = useState<string | null>(null);
+  useEffect(() => {
+    fetch("/api/v1/hello")
+      .then((res) => res.json())
+      .then(async (data) =>
+        setTimeout(() => {
+          setMessage(data);
+        }, 1000)
+      );
+  }, []);
   return (
     <>
       <Head>
-        <title>app hello hi</title>
+        <title>{message}</title>
       </Head>
-      <main className={styles.main}>hello</main>
+      <main className={styles.main}>{message ?? "loading"}</main>
     </>
   );
 }
