fbc81fa0cb5d3ecaf920750ca6dbcbf4c9c60478
chihabMg <chihab.mg.me@gmail.com>
2023-03-16 17:19:44 +0100

    expred the app from app.ts

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -3,15 +3,12 @@ import { redis_client_connect } from "./core/redis_client";
 import { createServer } from "./utils/server";
 
 const runServer = async () => {
-  try {
-    const app = createServer();
-    await redis_client_connect();
-    app.listen(env.PORT, () => {
-      console.log(`running the server on port ${env.PORT}`);
-    });
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
+  const app = createServer();
+  await redis_client_connect();
+  app.listen(env.PORT, () => {
+    console.log(`running the server on port ${env.PORT}`);
+  });
+  return app;
 };
 if (require.main == module) runServer();
+export default runServer();
