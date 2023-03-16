4bd1834a64909054cf27c60c20a520091210af7a
chihabMg <chihab.mg.me@gmail.com>
2023-03-16 16:02:12 +0100

    trying again

---
+++ apps/web/src/pages/api/hello.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "database";
+
+export default async function helloEndpoint(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const prisma = new PrismaClient();
+  try {
+    const users = await prisma.user.findMany();
+    return res.status(200).json(users);
+  } catch (er) {
+    console.error(er);
+    return res.status(400).json("error");
+  }
+}
