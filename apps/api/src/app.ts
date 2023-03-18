a0f667831c50b364857281ba7182f7d51c72c14e
chihabMg <chihab.mg.me@gmail.com>
2023-03-18 13:05:34 +0100

    added errors handler in runServer func

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -3,12 +3,17 @@ import { redis_client_connect } from "./core/redis_client";
 import { createServer } from "./utils/server";
 
 const runServer = async () => {
-  const app = createServer();
-  await redis_client_connect();
-  app.listen(env.PORT, () => {
-    console.log(`running the server on port ${env.PORT}`);
-  });
-  return app;
+  try {
+    const app = createServer();
+    await redis_client_connect();
+    app.listen(env.PORT, () => {
+      console.log(`running the server on port ${env.PORT}`);
+    });
+    return app;
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 };
 // if (require.main == module) runServer();
 export default runServer();
